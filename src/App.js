2be9a68f093bc7d103ade2ea9fc191027f23d706
kokanek <kapeel.kokane@gmail.com>
2021-02-23 19:57:30 +0530

    Home page done

--- src/App.js
+++ src/App.js
@@ -47,8 +47,23 @@ function App() {
   return (
     <div className="container">
       <div className="App">
+        <div className="emoji"><ion-icon name="heart-outline"></ion-icon><ion-icon name="person"></ion-icon></div>
         <h1>My Paper Card</h1>
-        <input
+        <button onClick={createNote} style={{marginBottom: 30, marginTop: 30}} className="btn-success">Create Card<ion-icon name="add-circle-outline" ></ion-icon></button>
+        {notes.length !== 0 && <div >
+          <p>Cards created by you: </p>
+          {
+            notes.map(note => (
+              <div key={note.id || note.name} className="cardListItem">
+                <h2 style={{marginBottom: 12, marginTop: 12}}>{note.name}</h2>
+                <p>{note.description}</p>
+                <button onClick={() => deleteNote(note)} className="btn-primary">View Card</button>
+              </div>
+            ))
+          }
+        </div>}
+        {notes.length === 0 && <p>You do not have any cards created</p>}
+        {/* <input
           onChange={e => setFormData({ ...formData, 'name': e.target.value})}
           placeholder="Note name"
           value={formData.name}
@@ -57,20 +72,22 @@ function App() {
           onChange={e => setFormData({ ...formData, 'description': e.target.value})}
           placeholder="Note description"
           value={formData.description}
-        />
-        <button onClick={createNote}>Create Note</button>
-        <div style={{marginBottom: 30}}>
-          {
-            notes.map(note => (
-              <div key={note.id || note.name}>
-                <h2>{note.name}</h2>
-                <p>{note.description}</p>
-                <button onClick={() => deleteNote(note)}>Delete note</button>
-              </div>
-            ))
-          }
+        /> */}
+        
+        <div class="row flex-spaces child-borders">
+          <label class="paper-btn margin btn-danger" for="modal-1">Sign Out</label>
         </div>
-        <AmplifySignOut />
+        <input class="modal-state" id="modal-1" type="checkbox" />
+        <div class="modal">
+          <label class="modal-bg" for="modal-1"></label>
+          <div class="modal-body">
+            <label class="btn-close" for="modal-1">X</label>
+            <h4 class="modal-title">Sign Out</h4>
+            <p class="modal-text">Are you sure you want to sign out?</p>
+            <label for="modal-1"><AmplifySignOut/></label>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
